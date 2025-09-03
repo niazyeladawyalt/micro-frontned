ca830fc9b83e00dd307b1b13099e6276a881d139
niazy <niazy@alternative.agency>
2025-09-03 10:57:46 +0400

    remove uneccessary files

--- remote-app/api/remote-entry.ts
+++ remote-app/api/remote-entry.ts
@@ -1,41 +1,41 @@
-// api/remote-entry.ts
-import path from "path";
-import fs from "fs";
+// // api/remote-entry.ts
+// import path from "path";
+// import fs from "fs";
 
-export default function handler(req, res) {
-  const allowedOrigins = ["https://micro-frontned-host.vercel.app"];
-  const origin = req.headers.origin || "";
+// export default function handler(req, res) {
+//   const allowedOrigins = ["https://micro-frontned-host.vercel.app"];
+//   const origin = req.headers.origin || "";
 
-  // Handle preflight
-  if (req.method === "OPTIONS") {
-    if (allowedOrigins.includes(origin)) {
-      res.setHeader("Access-Control-Allow-Origin", origin);
-      res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS");
-      res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-      res.status(200).end();
-      return;
-    } else {
-      res.status(403).end();
-      return;
-    }
-  }
+//   // Handle preflight
+//   if (req.method === "OPTIONS") {
+//     if (allowedOrigins.includes(origin)) {
+//       res.setHeader("Access-Control-Allow-Origin", origin);
+//       res.setHeader("Access-Control-Allow-Methods", "GET,OPTIONS");
+//       res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+//       res.status(200).end();
+//       return;
+//     } else {
+//       res.status(403).end();
+//       return;
+//     }
+//   }
 
-  if (!allowedOrigins.includes(origin)) {
-    return res.status(403).send("Forbidden");
-  }
+//   if (!allowedOrigins.includes(origin)) {
+//     return res.status(403).send("Forbidden");
+//   }
 
-  // Absolute path to your built remoteEntry.js
-  const filePath = path.join(process.cwd(), "public", "remoteEntry.js");
+//   // Absolute path to your built remoteEntry.js
+//   const filePath = path.join(process.cwd(), "public", "remoteEntry.js");
 
-  if (!fs.existsSync(filePath)) {
-    console.error("remoteEntry.js not found at", filePath);
-    return res.status(500).send("remoteEntry.js not found");
-  }
+//   if (!fs.existsSync(filePath)) {
+//     console.error("remoteEntry.js not found at", filePath);
+//     return res.status(500).send("remoteEntry.js not found");
+//   }
 
-  const fileContent = fs.readFileSync(filePath, "utf-8");
+//   const fileContent = fs.readFileSync(filePath, "utf-8");
 
-  res.setHeader("Access-Control-Allow-Origin", origin);
-  res.setHeader("Vary", "Origin");
-  res.setHeader("Content-Type", "application/javascript");
-  res.status(200).send(fileContent);
-}
+//   res.setHeader("Access-Control-Allow-Origin", origin);
+//   res.setHeader("Vary", "Origin");
+//   res.setHeader("Content-Type", "application/javascript");
+//   res.status(200).send(fileContent);
+// }
